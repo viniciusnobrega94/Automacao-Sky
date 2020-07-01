52eabd7754e9ac646e80a7d546e1a3668d083e5a
Vinicius Nobrega <viniciusonobrega@hotmail.com>
2020-07-01 18:38:11 -0300

    Projeto criado

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem "capybara", "3.31.0"
+gem "rspec", "3.9.0"
+gem "selenium-webdriver", "3.142.7"
+gem "ffi", "1.12.2"
+
---
+++ spec/TestSKY_spec.rb
@@ -0,0 +1,45 @@
+describe  "Teste SKY" do
+
+    before(:each) do
+        visit "https://www.sky.com.br/"
+    end
+
+    it 'Ver programação' do
+        
+    within('.modal-dialog') do
+            find('.sky_icon-close').click
+            end
+        
+
+    find('a[href="/programacao"]').click
+
+    descricao = 'a'
+    duracao = 'b'
+    descricaoModal = 'c'
+    duracaoModal = 'd'
+
+    within('.schedule-live', match: :first) do
+        descricao = find('.program-schedule-title').text
+        duracao = find('.program-duration').text
+        puts descricao
+        puts duracao
+        
+    end
+    
+    find('.schedule-live', match: :first).click
+
+    within('.sky-modal-program') do
+        descricaoModal = find('.sky-modal-program-title').text
+        duracaoModal = find('.sky-modal-program-date-time').text
+        puts descricaoModal
+        puts duracaoModal
+        
+    end
+    
+    expect(descricaoModal).to eql (descricao)
+    expect(duracaoModal).to eql (duracao)
+end
+    
+end
+
+    
